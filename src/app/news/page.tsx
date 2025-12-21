498b665fda1bb0bbb1d212c273062894ff419aa6
pranto113015 <pranto3876@gmail.com>
2025-12-21 15:18:32 +0600

    feat: create News Page component with initial structure

---
+++ src/app/news/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Newspage = () => {
+  return (
+    <div>
+      <div>
+        <h1>This is News Page</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Newspage
