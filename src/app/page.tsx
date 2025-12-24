d258d8961601473c2d86f70f4b5a2ea023c4844a
pranto113015 <pranto3876@gmail.com>
2025-12-25 00:42:06 +0600

    feat: enhance Home page layout and optimize Banner component code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import Banner from "@/components/shared/Banner";
 
-
 export default function Home() {
   return (
-<div className="py-12">
-  <Banner />
-</div>
+    <div className="py-12">
+      <Banner />
+
+      <div className="my-12">
+        <h2 className="text-2xl font-bold mb-8">Latest News</h2>
+      </div>
+    </div>
   );
 }
