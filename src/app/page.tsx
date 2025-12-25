c5cc3f31acbab5e2e47662d4858f3a92ce0802b1
pranto113015 <pranto3876@gmail.com>
2025-12-26 02:18:03 +0600

    feat: add NewsCard component and News types
    - Implemented NewsCard component for displaying news items with image, title, and description.
    - Added NewsItem and NewsCardProps interfaces to define the structure of news data.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,24 @@
 import Banner from "@/components/shared/Banner";
+import NewsCard from "@/components/shared/NewsCard";
+import { NewsItem } from "@/types/news";
 
-export default function Home() {
+const Home = async () => {
+  const data = await fetch("https://demo-news-api.vercel.app/api/news");
+  const news = await data.json();
+
   return (
     <div className="py-12">
       <Banner />
 
       <div className="my-12">
         <h2 className="text-2xl font-bold mb-8">Latest News</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          {news.slice(0, 3).map((item: NewsItem) => (
+            <NewsCard key={item?._id} item={item} />
+          ))}
+        </div>
       </div>
     </div>
   );
-}
+};
+export default Home;
